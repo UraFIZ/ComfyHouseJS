cfe828ca4d13510daa92a9e86c7bcc22dd772745
UraFIZ <kratser88@gmail.com>
2019-04-05 00:56:01 +0300

    displayed carts

--- app.js
+++ app.js
@@ -19,9 +19,56 @@ console.log(productsDOM);
 
 let cart = [];
 //getting data
-class Products {}
+class Products {
+  async getProducts() {
+    try {
+      let result = await fetch('products.json');
+      let data = await result.json();
+      let products = data.items;
+      products = products.map(item => {
+        const { title, price } = item.fields;
+        const { id } = item.sys;
+        const {
+          image: {
+            fields: {
+              file: { url },
+            },
+          },
+        } = item.fields;
+        return { title, price, id, url };
+      });
+      return products;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
 //display products
-class UI {}
+class UI {
+  displayProducts(products) {
+    let result = '';
+    products.forEach(item => {
+      result += `
+      <arctical class="product"> 
+          <div class="img-container">
+            <img
+              src=${item.url}
+              alt="product"
+              class="product-img"
+            />
+            <button class="bag-btn" data-id=${item.id}>
+              <i class="fas fa-shopping-cart"></i>
+              add to bag
+            </button>
+          </div>
+          <h3>${item.title}</h3>
+          <h4>$${item.price}</h4>
+          </arctical>
+    `;
+    });
+    productsDOM.innerHTML = result;
+  }
+}
 
 //local storage
 
@@ -30,4 +77,6 @@ class Storage {}
 document.addEventListener('DOMContentLoaded', () => {
   const ui = new UI();
   const products = new Products();
+  //get all products
+  products.getProducts().then(products => ui.displayProducts(products));
 });
