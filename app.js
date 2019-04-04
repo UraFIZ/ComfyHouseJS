7c115874bcd04e744a5535fc0bd81c76ff3bb118
UraFIZ <kratser88@gmail.com>
2019-04-05 01:44:26 +0300

    saved data in localStorage

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
 const cartBtn = document.querySelector('.cart-btn');
-console.log(cartBtn);
 const closeCartBtn = document.querySelector('.close-cart');
-console.log(closeCartBtn);
 const clearCartBtn = document.querySelector('.clear-cart');
-console.log(clearCartBtn);
 const cartDOM = document.querySelector('.cart');
-console.log(cartDOM);
 const cartOverlay = document.querySelector('.cart-overlay');
-console.log(cartOverlay);
 const cartItems = document.querySelector('.cart-items');
-console.log(cartItems);
 const cartTotal = document.querySelector('.cart-total');
-console.log(cartTotal);
 const cartContent = document.querySelector('.cart-content');
-console.log(cartContent);
 const productsDOM = document.querySelector('.products-center');
-console.log(productsDOM);
 
 let cart = [];
 //getting data
@@ -72,11 +63,18 @@ class UI {
 
 //local storage
 
-class Storage {}
-
+class Storage {
+  static saveProducts(products) {
+    localStorage.setItem('products', JSON.stringify(products));
+  }
+}
+console.log(Storage.saveProducts());
 document.addEventListener('DOMContentLoaded', () => {
   const ui = new UI();
   const products = new Products();
   //get all products
-  products.getProducts().then(products => ui.displayProducts(products));
+  products.getProducts().then(products => {
+    ui.displayProducts(products);
+    Storage.saveProducts(products);
+  });
 });
