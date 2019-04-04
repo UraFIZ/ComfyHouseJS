4ce7b7c684ded7a0637912bcb250fc83a79bdd81
UraFIZ <kratser88@gmail.com>
2019-04-04 17:41:13 +0300

    js init settings

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('object');
+const cartBtn = document.querySelector('.cart-btn');
+console.log(cartBtn);
+const closeCartBtn = document.querySelector('.close-cart');
+console.log(closeCartBtn);
+const clearCartBtn = document.querySelector('.clear-cart');
+console.log(clearCartBtn);
+const cartDOM = document.querySelector('.cart');
+console.log(cartDOM);
+const cartOverlay = document.querySelector('.cart-overlay');
+console.log(cartOverlay);
+const cartItems = document.querySelector('.cart-items');
+console.log(cartItems);
+const cartTotal = document.querySelector('.cart-total');
+console.log(cartTotal);
+const cartContent = document.querySelector('.cart-content');
+console.log(cartContent);
+const productsDOM = document.querySelector('.products-center');
+console.log(productsDOM);
+
+let cart = [];
+//getting data
+class Products {}
+//display products
+class UI {}
+
+//local storage
+
+class Storage {}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const ui = new UI();
+  const products = new Products();
+});
