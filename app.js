2e6a837da2659548c8585ed9c24be63cc76edc3c
UraFIZ <kratser88@gmail.com>
2019-04-05 17:07:15 +0300

    i cant display backpanel

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const cartContent = document.querySelector('.cart-content');
 const productsDOM = document.querySelector('.products-center');
 
 let cart = [];
+let buttonsDOM = [];
 //getting data
 class Products {
   async getProducts() {
@@ -59,6 +60,69 @@ class UI {
     });
     productsDOM.innerHTML = result;
   }
+  getBagButtons() {
+    const buttons = [...document.querySelectorAll('.bag-btn')];
+    buttonsDOM = buttons;
+    buttons.forEach(button => {
+      let id = button.dataset.id;
+      let inCart = cart.map(itme => itme.id === id); /* Storage.getCart(id); */
+      if (inCart) {
+        button.innerText = 'In Cart';
+        button.disabled = true;
+      }
+      button.addEventListener('click', event => {
+        event.target.innerText = 'In Cart';
+        event.target.disabled = true;
+        //get product from products
+        let cartItem = { ...Storage.getProduct(id), amount: 1 };
+        //add product to the cart
+        cart = [...cart, cartItem];
+        //save cart in local storage
+        Storage.saveCart(cart);
+        //set cart values
+        this.setCartValue(cart);
+        //display cart item
+        this.addCartItem(cartItem);
+        //show the cart
+        this.showCart();
+      });
+    });
+  }
+  setCartValue(cart) {
+    let tempTotal = 0;
+    let itemsTotal = 0;
+    cart.map(item => {
+      tempTotal += item.price * item.amount;
+      itemsTotal += item.amount;
+    });
+    cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+    cartItems.innerText = itemsTotal;
+  }
+  addCartItem(item) {
+    const div = document.createElement('div');
+    div.classList.add('cart-item');
+    div.innerHTML = `<img src=${item.url} alt="product" srcset="" />
+
+      <div>
+        <h4>${item.title}</h4>
+        <h5>$${item.price}</h5>
+
+        <span class="remove-item" data-id=${item.id}>remove</span>
+      </div>
+      <div>
+        <i class="fas fa-chevron-up"data-id=${item.id}></i>
+        <p class="item-amount"data-id=${item.id}>${item.amount}</p>
+        <i class="fas fa-chevron-down"data-id=${item.id}></i>
+      </div>
+              </div >`;
+    cartContent.appendChild(div);
+  }
+  showCart() {
+    cartOverlay.classList.add('transparentBcg');
+    console.log(cartOverlay);
+    cartDOM.classList.add('showCart');
+    console.log(cartDOM);
+  }
 }
 
 //local storage
@@ -67,14 +131,29 @@ class Storage {
   static saveProducts(products) {
     localStorage.setItem('products', JSON.stringify(products));
   }
+  static getProduct(id) {
+    let products = JSON.parse(localStorage.getItem('products'));
+    return products.find(product => product.id === id);
+  }
+  static saveCart(cart) {
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }
+  static getCart(id) {
+    let cart = JSON.parse(localStorage.getItem('cart'));
+    return cart.find(item => item.id === id);
+  }
 }
-console.log(Storage.saveProducts());
 document.addEventListener('DOMContentLoaded', () => {
   const ui = new UI();
   const products = new Products();
   //get all products
-  products.getProducts().then(products => {
-    ui.displayProducts(products);
-    Storage.saveProducts(products);
-  });
+  products
+    .getProducts()
+    .then(products => {
+      ui.displayProducts(products);
+      Storage.saveProducts(products);
+    })
+    .then(() => {
+      ui.getBagButtons();
+    });
 });
